dc15b2869ba5f4003d11b7c06ffddeb8b2056f1c
rmtrigo <rmontrigo@gmail.com>
2015-07-07 21:43:00 -0700

    Commit 7: Java Data Structures - Earned Java Collection Badge

--- java_data_structures/Example.java
+++ java_data_structures/Example.java
@@ -1,5 +1,12 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.Set;
 
 import com.teamtreehouse.Treet;
 import com.teamtreehouse.Treets;
@@ -8,34 +15,46 @@ import com.teamtreehouse.Treets;
 public class Example {
 
 	public static void main(String[] args) {
-		// Treet treet = new Treet(
-		// 	"craigsdennis",
-		// 	"Want to be famous? Simply tweet about Java and use " + 
-		// 	"the hashtag #treet. I’ll use your tweet in a new " +
-		// 	" @treehouse course about data structures.",
-		// 	new Date(1421849732000L)
-		// );
-		// Treet secondTreet = new Treet(
-		// 	"journeytocode",
-		// 	"@treehouse makes learning Java sooooo fun! #treet",
-		// 	new Date(1421849732000L)
-		// );
-		// System.out.printf("This is a new treet: %s %n", treet);
-		// System.out.printf("The words are:");
-		// for (String word : treet.getWords()) {
-		// 	System.out.println(word);
-		// }
-		// Treet[] treets = {treet, secondTreet};
-		// Arrays.sort(treets);
-		// for (Treet exampleTreet : treets) {
-		// 	System.out.println(exampleTreet);
-		// }
-		// Treets.save(treets);
-		Treet[] reloadedTreets = Treets.load();
-		for (Treet reloaded : reloadedTreets) {
-			System.out.println(reloaded);
+		Treet[] treets = Treets.load();
+		System.out.printf("There are %d treets. %n", 
+						treets.length);
+		Set<String> allHashTags = new HashSet<String>();
+		Set<String> allMentions = new TreeSet<String>();
+		for (Treet treet : treets) {
+			allHashTags.addAll(treet.getHashTags());
+			allHashTags.addAll(treet.getMentions());
 		}
-		// Treet[] treets = Treets.load();
-		// System.out.printf("There are %d treets. %n", treets.length);
+		System.out.printf("Hash tags: %s %n", allHashTags);
+		System.out.printf("Mentions: %s %n", allMentions);
+
+		Map<String, Integer> hashTagCounts = 
+			new HashMap<String, Integer>();
+		for (Treet treet : treets) {
+			for (String hashTag : treet.getHashTags()) {
+				Integer count = hashTagCounts.get(hashTag);
+				if (count == null) {
+					count = 0;
+				}
+				count++;
+				hashTagCounts.put(hashTag, count);
+			}
+		}
+		System.out.printf("Hash tag counts: %s %n", hashTagCounts);
+
+		Map<String, List<Treet>> treetsByAuthor = new 
+			HashMap<String, List<Treet>>();
+		for (Treet treet : treets) {
+			List<Treet> authoredTreets = treetsByAuthor
+				.get(treet.getAuthor());
+			if (authoredTreets == null) {
+				authoredTreets = new ArrayList<Treet>();
+				treetsByAuthor.put(treet.getAuthor(), authoredTreets);
+			}
+			authoredTreets.add(treet);
+		}
+
+		System.out.printf("Treets by author: %s %n ", treetsByAuthor);
+		System.out.printf("Treets by nickrp: %s %n ", 
+						treetsByAuthor.get("nickrp"));
 	}
 }
--- java_data_structures/com/teamtreehouse/Treet.java
+++ java_data_structures/com/teamtreehouse/Treet.java
@@ -1,9 +1,12 @@
 package com.teamtreehouse;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
-public class Treet implements Comparable, Serializable {
+public class Treet implements Comparable<Treet>, Serializable {
 	private static final long serialVersionUID = 7146681148113043748L;
 	private String mAuthor;
 	private String mDescription;
@@ -22,8 +25,7 @@ public class Treet implements Comparable, Serializable {
 	}
 
 	@Override
-	public int compareTo(Object obj) {
-		Treet other = (Treet) obj;
+	public int compareTo(Treet other) {
 		if (equals(other)) {
 			return 0;
 		}
@@ -46,7 +48,26 @@ public class Treet implements Comparable, Serializable {
 		return mCreationDate;
 	}
 
-	public String[] getWords() {
-		return mDescription.toLowerCase().split("[^\\w#@']+");
+	public List<String> getWords() {
+		String[] words = mDescription.toLowerCase().split("[^\\w#@']+");
+		return Arrays.asList(words);
+	}
+
+	public List<String> getHashTags() {
+		return getWordsPrefixedWith("#");
+	}
+
+	public List<String> getMentions() {
+		return getWordsPrefixedWith("@");
+	}
+
+	private List<String> getWordsPrefixedWith(String prefix) {
+		List<String> results = new ArrayList<String>();
+		for (String word : getWords()) {
+			if (word.startsWith(prefix)) {
+				results.add(word);
+			}
+		}
+		return results;
 	}
 }
